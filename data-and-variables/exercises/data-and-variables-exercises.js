84634273d47cae959b5462ae089bef48519844a2
christy osakwe <you@example.com>
2024-01-10 19:32:15 -0600

    fighting git

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,4 +1,10 @@
 // Declare and assign the variables below
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621; 
+let milesToMars = (distanceToMarsKm * milesPerKm);
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
 
