d48a86831786bda29d8ef8e80964f57b0dc4204a
kristyneka <kristyneka@gmail.com>
2024-01-23 20:50:05 -0600

    update

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,11 +2,11 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
-let beverages = [];
-let desserts = [];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
+let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
 
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
@@ -15,7 +15,14 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-
+  for (let i = 0; i < numMeals; i++) {
+    let mealbox = []; 
+    for (let j = 0; j < pantry.length ; j++) {
+      mealbox.push(pantry[j][i]);
+      // console.log(mealbox)
+    } 
+      meals.push(mealbox);
+ }
 
   return meals;
 }
@@ -23,7 +30,10 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
 
 function askForNumber() {
   numMeals = input.question("How many meals would you like to make?");
-  
+  while (numMeals < 1 || numMeals > 6 || isNaN(numMeals)) {
+    numMeals = input.question("How many meals would you like to make? ") ;}
+    return numMeals;
+  }
   /// CODE YOUR SOLUTION TO PART B here ///
 
   return numMeals;
