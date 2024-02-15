7b540d63fd3675d957e5a59b2d00e887aa15790e
kristyneka <kristyneka@gmail.com>
2024-02-14 19:34:42 -0600

    object

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -4,6 +4,11 @@ let superChimpOne = {
    mass: 9,
    age: 6
 };
+superChimpOne.astronautId = 10;
+superChimpOne.move = function () {
+    return Math.ceil(Math.random()*10);
+      
+}
 
 let salamander = {
    name: "Lacey",
@@ -14,37 +19,82 @@ let salamander = {
 salamander.astronautId = 8;
 console.log(salamander);
 
-let Chip = {
+salamander.move = function () {
+   return Math.ceil(Math.random()*10);
+     
+}
+
+
+let chip = {
    name: "Brad",
    species: "Chimpanzee",
    mass: 11,
    age: 6,
 };
-Chip.astronautId = 5;
-console.log(Chip);
+chip.astronautId = 5;
+console.log(chip);
+chip.move = function () {
+   return Math.ceil(Math.random()*10);
+     
+}
 
-let Bea = {
+let bea = {
    name: "Leroy",
    species: "Beagle",
    mass: 14,
    age: 5,
 };
-Bea.astronautId = 2;
-console.log(Bea);
+bea.astronautId = 2;
+console.log(bea);
+bea.move = function () {
+   return Math.ceil(Math.random()*10);
+     
+}
 
-let Tardi = {
+let tardi = {
    name: "Almina",
    species: "Tardigrade",
    mass: 0.0000000001,
    age: 1,
 };
-Tardi.astronautId = 9;
-console.log(Tardi);
-Tardi {Tardi(Almina) {
-   this.name = 
+tardi.astronautId = 9;
+console.log(tardi);
+tardi.move = function () {
+   return Math.ceil(Math.random()*10);
+     
 }
 
+let animalObject = [superChimpOne, salamander, chip, bea, tardi];
+
+function crewReports(object) {
+   return `${object["name"]} is a ${object["species"]}. They are ${object["age"]} years old and ${object.mass} kilograms. Their ID is ${object["astronautId"]}.`
+   // Fill in the blanks with the name, species, age, mass, and ID for the selected animal.
 }
+
+
+console.log(crewReports(animalObject[0]));
+console.log(crewReports(animalObject[1]));
+console.log(crewReports(animalObject[2]));
+console.log(crewReports(animalObject[3]));
+console.log(crewReports(animalObject[4]));
+
+
+
+function fitnessTest(animals){
+   let results = [];
+   for (let i = 0; i < animals.length; i++){
+       let numSteps = 0;
+       let turns = 0;
+       while(numSteps <= 20){
+       numSteps += animals[i].move();
+       turns++;
+       }
+       results.push(`${animals[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+  }
+console.log(animalObject[0]);
+console.log(fitnessTest(animalObject));
 // After you have created the other object literals, add the astronautID property to each one.
 
 // Create an array to hold the animal objects.
